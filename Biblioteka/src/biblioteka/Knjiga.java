2c36e8afb29edc3627b2ed441a6e7c8d6e9f8e7f
Markec19 <markovicl136@gmail.com>
2023-02-28 11:33:51 +0100

    Uvedeni toString, hashCode i equals za klase Autor i Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -49,5 +49,13 @@ public class Knjiga {
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
+	
+	
 	
 }
